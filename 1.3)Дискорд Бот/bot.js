4245d52dcb5cb2617c32d110585ab09ce077aec7
nekokolurka <72972407+nekokolurka@users.noreply.github.com>
2021-12-12 02:57:18 +0300

    Update bot.js

--- 1.3)Дискорд Бот/bot.js
+++ 1.3)Дискорд Бот/bot.js
@@ -1,1 +1,31 @@
+const { Client, Intents } = require('discord.js');
+const Discord = require('discord.js'); 
+const robot = new Client({ intents: [Intents.FLAGS.GUILDS] }); 
+const comms = require("./comms.js"); 
+const fs = require('fs');   
+let config = require('./config.json'); 
+let token = config.token; 
+let prefix = config.prefix; 
 
+robot.on("ready", function() {
+  
+  console.log(robot.user.username + " запустился!");
+});
+
+
+robot.on('message', (msg) => { 
+  if (msg.author.username != robot.user.username && msg.author.discriminator != robot.user.discriminator) {
+    var comm = msg.content.trim() + " ";
+    var comm_name = comm.slice(0, comm.indexOf(" "));
+    var messArr = comm.split(" ");
+    for (0 in comms.comms) {
+      var comm2 = prefix + comms.comms[0].name;
+      if (comm2 == comm_name) {
+        comms.comms[0].out(robot, msg, messArr);
+      }
+    }
+  }
+});
+
+
+robot.login(token); 
